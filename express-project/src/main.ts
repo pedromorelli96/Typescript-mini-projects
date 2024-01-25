a389fcef074fea6c94349920caaab8c09aa9344c
Pedro Morelli <pedromorelli96@gmail.com>
2024-01-25 19:55:24 -0300

    Adds zod and middleware to express-project

--- express-project/src/main.ts
+++ express-project/src/main.ts
@@ -1,30 +1,59 @@
-import express, { type Application, type Request, type Response } from "express";
+import express, { type NextFunction, type Application, type Request, type Response } from "express";
+import { z } from "zod";
 
 const app: Application = express();
 
-const users: Array<{
-  id: number;
-  name: string;
-}> = [
-  {
-    id: 1,
-    name: "John Doe",
-  },
-  {
-    id: 2,
-    name: "Jane Doe",
-  },
-  {
-    id: 3,
-    name: "John Smith",
-  },
-];
+const userSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+    })
+    .email("Email must have a valid email format"),
+  username: z
+    .string({
+      required_error: "Username is required",
+    })
+    .trim()
+    .min(5)
+    .max(20),
+});
+
+type User = z.infer<typeof userSchema>;
+
+const users: User[] = [];
 
 app.use(express.json());
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.get("/", (req: Request, res: Response) => {
-  const id = req.body.id;
-  res.send(users.filter((user) => user.id === id));
+  res.status(200).send(users);
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+app.post("/", (req: Request, res: Response) => {
+  const newUser = userSchema.parse(req.body);
+  users.push(newUser);
+  res.status(201).send(newUser);
+});
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: unknown, req: Request, res: Response, _next: NextFunction) => {
+  if (err instanceof z.ZodError) {
+    res.status(400).json({
+      error: err.flatten(),
+    });
+    return;
+  } else if (err instanceof Error) {
+    const error = err as Error & { statusCode?: number };
+    res.status(error.statusCode ?? 400).json({
+      message: err.message,
+    });
+    return;
+  }
+  res.status(500).json({
+    message: "Internal server error",
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Server running on port 5000");
+});
